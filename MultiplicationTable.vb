a66fd2669053361179eda6a110391941460896db
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-13 18:29:13 -0600

    Initial Commit

--- MultiplicationTable.vb
+++ MultiplicationTable.vb
@@ -12,11 +12,31 @@ Module MultiplicationTable
     Sub Main()
         Dim numberInput, column, row As Integer
         Dim data(12, 12) As Integer
+        Dim goodData As Boolean
+        Dim quit As String
+        Dim userInput As String
 
         Do
-            Console.WriteLine("Please Enter a Number:")
+            Do
+                Console.WriteLine("Please Enter a Number:")
 
-            numberInput = CInt(Console.ReadLine())
+                Try
+                    userInput = Console.ReadLine()
+                    numberInput = CInt(userInput)
+                    If numberInput < 13 And numberInput > 0 Then
+                        goodData = True
+                    Else
+                        goodData = False
+                        Console.WriteLine("Only 1 to 12 will work boi")
+                    End If
+                Catch ex As Exception
+                    If userInput = "q" Then
+                        Exit Sub
+                    End If
+                    Console.WriteLine("Only 1 to 12 will work boi")
+                    goodData = False
+                End Try
+            Loop Until goodData = True
 
             For column = 1 To numberInput
                 For row = 1 To numberInput
@@ -25,13 +45,12 @@ Module MultiplicationTable
                 Next
                 Console.WriteLine()
             Next
-
-            Console.ReadLine()
+            Console.WriteLine("Press Enter to start over or Q to quit.")
+            quit = Console.ReadLine()
+            If quit = "q" Then
+                Exit Sub
+            End If
         Loop
-
-
-
-
     End Sub
 
 End Module
